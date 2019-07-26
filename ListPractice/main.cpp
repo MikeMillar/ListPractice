321aa2a1306995dba281a97df993d29f556c30e6
Mike Millar <39285355+MikeMillar@users.noreply.github.com>
2019-07-25 22:39:22 -0600

    add list, push back, front, insert, looping through list

---
+++ ListPractice/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+int main() {
+
+	list<int> numbers;
+
+	numbers.push_back(1);
+	numbers.push_back(2);
+	numbers.push_back(3);
+	numbers.push_front(0);
+
+	list<int>::iterator it = numbers.begin();
+	cout << "Element: " << *it << endl;
+	it++;
+	cout << "Element: " << *it << endl;
+	numbers.insert(it, 100);
+	cout << "Element: " << *it << endl;
+
+	for (list<int>::iterator it = numbers.begin(); it != numbers.end(); it++) {
+		cout << *it << endl;
+ 	}
+
+	return 0;
+}
